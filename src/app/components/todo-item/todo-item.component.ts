5235839235597c89a81a8f499e01c795b8211903
Ian <imatlak03@gmail.com>
2021-03-02 01:01:45 -0500

    maps obj and added dynamic class binding

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
+  // Input property
+  @Input() todo: Todo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  // Set Dynamic Classes
+  // todo class always applied
+  // is completed only added if input box checked
+  setClasses() {
+    let classes = {
+      todo: true,
+      // in reference to the todo passed in from the input
+      'is-complete': this.todo.completed,
+    };
+
+    return classes;
   }
-
 }
